7f1825662f490950dc8aa288798e1978a24d8cc7
Dharani 007 <dharanimeenatchisundharam112@gmail.com>
2023-02-23 23:36:25 +0530

    ADDED : Hello kartikeya

--- src/Demo.java
+++ src/Demo.java
@@ -1,6 +1,5 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("hello java");
-        System.out.println("Hello kartikeya");
+        System.out.println("Hello Dharani");
     }
 }
